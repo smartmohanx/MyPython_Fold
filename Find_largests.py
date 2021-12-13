c8e8be5e54691e3f4d102b6856a9a8a03442106b
Sigiti Mohan Rao <72431019+smartmohanx@users.noreply.github.com>
2021-12-13 11:30:16 +0530

    Add files via upload

---
+++ Find_largests.py
@@ -0,0 +1,32 @@
+
+#Final largest number!
+
+#your code goes here
+
+lis=[2,55,88,6,99,34,1]
+
+
+def find_larg(lis,n):
+	max=lis[0]
+	for i in range(1,n):
+		if lis[i]>max:
+			max=lis[i]
+	return max
+	
+n=len(lis)
+ans=find_larg(lis,n)
+print(lis)
+print("The largest:",ans)
+
+lis.remove(ans)
+
+n=len(lis)
+s_l=find_larg(lis,n)
+print("2nd largest:",s_l)
+
+
+lis.remove(s_l)
+
+n=len(lis)
+t_l=find_larg(lis,n)
+print("3rd largest:",t_l)
